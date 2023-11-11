c0947559db3cf95ca8959e8d1abac58e07a6ee05
Devarshi Goswami <devarshigoswami@Devarshis-MacBook-Air.local>
2023-11-11 15:13:07 +0000

    final iteration

--- FinalExperiment.py
+++ FinalExperiment.py
@@ -1,58 +1,91 @@
 import pandas as pd
 from GeneticAlgorithm import GeneticAlgorithm
 from ReadXml import read_xml
-import statistics
 from tqdm import tqdm
 
-# Read the XML file and parse it into a DataFrame
+
+# single time setup
 xml_file_path = '/Users/devarshigoswami/Desktop/brazil58.xml'
-
-df = read_xml(xml_file_path)
-
-# Now you have the parsed DataFrame to work with
-print(df)
-
-n = 50  # The number of permutations to generate
-tournament_size = 2  # The size of the tournament for selection
+distance_matrix = read_xml(xml_file_path)
+print(distance_matrix)
+print(distance_matrix.shape)
 input_string = [i for i in range(58)]
 
-log_file = open("genetic_algorithm_log.txt", "w")
 
-# Initialize the GeneticAlgorithm object
-ga = GeneticAlgorithm(input_string, n, tournament_size, df,1)
-print('candidates', ga.candidates.sort_values('Cost'))
-log_file.write(f"{ga.candidates.sort_values('Cost')}\n")
-log_file.flush()
-# Call methods on the object for your experiment
-counter=0
-try:
-    for i in tqdm(range(10000)):
-        selected_permutations = ga.tournament_selection()
-        log_file.write(f"selected permuations for iteration {i} {selected_permutations}\n")
-        log_file.flush()
-        candidate1 = selected_permutations[0]
-        candidate2 = selected_permutations[1]
-        log_file.write(f"candidates for iteration {i} {candidate1,candidate2}\n")
-        log_file.flush()
-        offspring1, offspring2 = ga.single_point_crossover(candidate1, candidate2)
-        if offspring1==candidate1 or offspring2==candidate2:
-            counter+=1
-            log_file.write(f"offsprings same as parent for iteration {i} {offspring1,offspring2}\n")
+def run_experiment(input_string,n,tournament_size,iterations,distance_matrix,mutation_point,crossover_type,mutation):
+    #create a log file for each experiment
+    log_file = open(f"genetic_algorithm_log.txt_{n}_{iterations}_{tournament_size}.txt", "w")
+    ga = GeneticAlgorithm(input_string, n, tournament_size, distance_matrix,mutation_point,crossover_type)
+    print('candidates', ga.candidates.sort_values('Cost'))
+    counter=0
+    iteration_list = []
+    min_cost_list = []
+
+    experiment_results = pd.DataFrame(columns=['Iteration', 'MinCost'])
+    try:
+        for i in tqdm(range(iterations)):
+            # select individuals=tournament size , then get the min cost
+            selected_permutations_1 = ga.tournament_selection()
+            selected_permutations_2 = ga.tournament_selection()
+
+            log_file.write(f"selected permuations for iteration {i} {selected_permutations_1}\n")
+            log_file.write(f"selected permuations for iteration {i} {selected_permutations_2}\n")
+
             log_file.flush()
-        log_file.write(f"offsprings for iteration {i} {offspring1,offspring2}\n")
-        log_file.flush()
-        mutated_offspring1 = ga.single_point_swap_mutation(offspring1)
-        mutated_offspring2 = ga.single_point_swap_mutation(offspring2)
-        log_file.write(f"mutated offspring for iteration {i} {mutated_offspring1,mutated_offspring2}\n")
-        log_file.flush()
-        ga.replace_highest_cost_solution(mutated_offspring1)
-        ga.replace_highest_cost_solution(mutated_offspring2)
-        log_file.write(f"{i} {min(ga.candidates.Cost.to_list())}\n")
-        log_file.flush()  # Flush the buffer to ensure immediate write
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+            candidate1 = selected_permutations_1
+            candidate2 = selected_permutations_2
+            log_file.write(f"candidates for iteration {i} {candidate1,candidate2}\n")
+            log_file.flush()
+            offspring1, offspring2 = ga.single_point_crossover(candidate1, candidate2)
+            if offspring1==candidate1 or offspring2==candidate2:
+                counter+=1
+                log_file.write(f"offsprings same as parent for iteration {i} {offspring1,offspring2}\n")
+                log_file.flush()
+            log_file.write(f"offsprings for iteration {i} {offspring1,offspring2}\n")
+            log_file.flush()
+            if mutation==True:
+                mutated_offspring1 = ga.single_point_swap_mutation(offspring1)
+                mutated_offspring2 = ga.single_point_swap_mutation(offspring2)
+                log_file.write(f"mutated offspring for iteration {i} {mutated_offspring1,mutated_offspring2}\n")
+                log_file.flush()
+                ga.replace_highest_cost_solution(mutated_offspring1)
+                ga.replace_highest_cost_solution(mutated_offspring2)
+            ga.replace_highest_cost_solution(offspring1)
+            ga.replace_highest_cost_solution(offspring2)
+            log_file.write(f"{i} {min(ga.candidates.Cost.to_list())}\n")
+            min_cost=min(ga.candidates.Cost.to_list())
+            iteration_list.append(i)
+            min_cost_list.append(min_cost)
+            log_file.flush()  # Flush the buffer to ensure immediate write
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
-log_file.close()
+    log_file.close()
+    experiment_results = pd.DataFrame({'Iteration': iteration_list, 'MinCost': min_cost_list})
 
-print(min(ga.candidates.Cost.to_list()))
-print(counter)
+    print('minimum of experiment',min(ga.candidates.Cost.to_list()))
+    experiment_results.to_csv(f'experiment_results_pop_size_{n}_tournament_size_{tournament_size}_iterations_{iterations}_crossover_type_{crossover_type}.csv', index=False)
+    print(counter)
+
+
+# run_experiment(input_string=input_string,n=50,tournament_size=2,iterations=50,distance_matrix=distance_matrix,mutation_point=1,crossover_type='fix')
+
+
+
+
+from itertools import product
+
+# Define the parameter ranges
+n_values = [50, 100, 1000, 5000, 10000]
+tournament_size_values = [2, 10, 20, 100, 1000]
+iterations_values = [10000, 20000, 30000]
+mutation_point_values = [1, 3, 5, 7]
+crossover_type_values = ['fix', 'ordered']
+mutation_true_values= [True,False]
+# Generate all possible parameter combinations
+parameter_combinations = product(n_values, tournament_size_values, iterations_values, mutation_point_values, crossover_type_values,mutation_true_values)
+
+# Run experiments for each parameter combination
+for n, tournament_size, iterations, mutation_point, crossover_type ,mutation in parameter_combinations:
+    print('experiment>>>>>>',n, tournament_size, iterations, mutation_point, crossover_type, mutation)
+    run_experiment(input_string=input_string, n=n, tournament_size=tournament_size, iterations=iterations, distance_matrix=distance_matrix, mutation_point=mutation_point, crossover_type=crossover_type,mutation=mutation)
--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -3,13 +3,14 @@ import numpy as np
 import pandas as pd
 
 class GeneticAlgorithm:
-    def __init__(self, input_vector, n, tournament_size, distance_matrix, mutation_point):
+    def __init__(self, input_vector, n, tournament_size, distance_matrix, mutation_point, crossover_type='fix'):
         self.mutation_point = mutation_point
         self.input_vector = input_vector
         self.n = n
         self.tournament_size = tournament_size
         self.distance_matrix = distance_matrix
         self.locations = len(input_vector)
+        self.crossover_type = crossover_type
         self.candidates = self.generate_permutations()
 
     def calculate_total_cost(self, input_vector):
@@ -34,10 +35,12 @@ class GeneticAlgorithm:
         candidates = pd.DataFrame(permutations, columns=['Candidate_Solution', 'Cost'])
         return candidates
 
-    def tournament_selection(self, tournament_size=2):
+    def tournament_selection(self, tournament_size):
         selected_indices = random.sample(range(len(self.candidates)), tournament_size)
-        selected_candidates = self.candidates.loc[selected_indices]
-        return selected_candidates['Candidate_Solution'].tolist()
+        selected_candidates = self.candidates.loc[selected_indices].sort_values('Cost',ascending=True).reset_index(drop=True)
+        # Sort the selected candidates based on their already calculated cost
+
+        return selected_candidates['Candidate_Solution'][0]
 
     def repair_offspring(self, offspring, candidate):
         seen = set()
@@ -61,13 +64,51 @@ class GeneticAlgorithm:
         return offspring
 
     def single_point_crossover(self, candidate1, candidate2):
-
         crossover_point = random.randint(1, len(candidate1) - 1)
         offspring1 = candidate1[:crossover_point] + candidate2[crossover_point:]
         offspring2 = candidate2[:crossover_point] + candidate1[crossover_point:]
         offspring1 = self.repair_offspring(offspring1, candidate1)
         offspring2 = self.repair_offspring(offspring2, candidate2)
         return offspring1, offspring2
+
+    def ordered_crossover(self, parent1, parent2):
+        # Randomly select two crossover points (indices)
+        crossover_points = sorted(random.sample(range(len(parent1)), 2))
+
+        # Create empty offspring with the same length as the parents
+        offspring1 = [None] * len(parent1)
+        offspring2 = [None] * len(parent1)
+
+        # Copy a segment from the first parent to the offspring
+        offspring1[crossover_points[0]:crossover_points[1] + 1] = parent1[crossover_points[0]:crossover_points[1] + 1]
+        offspring2[crossover_points[0]:crossover_points[1] + 1] = parent2[crossover_points[0]:crossover_points[1] + 1]
+
+        # Create lists of elements remaining in parent2 (excluding the copied segment)
+        remaining_elements1 = [gene for gene in parent2 if gene not in offspring1]
+        remaining_elements2 = [gene for gene in parent1 if gene not in offspring2]
+
+        # Iterate through the offspring and fill in the remaining elements from parent2 and parent1
+        offspring_index1 = crossover_points[1] + 1
+        offspring_index2 = crossover_points[1] + 1
+
+        for gene1, gene2 in zip(remaining_elements1, remaining_elements2):
+            if offspring1[offspring_index1] is None:
+                offspring1[offspring_index1] = gene1
+            if offspring2[offspring_index2] is None:
+                offspring2[offspring_index2] = gene2
+            offspring_index1 = (offspring_index1 + 1) % len(parent1)
+            offspring_index2 = (offspring_index2 + 1) % len(parent1)
+
+        return offspring1, offspring2
+
+
+    def perform_crossover(self, candidate1, candidate2):
+        if self.crossover_type == 'fix':
+            return self.single_point_crossover(candidate1, candidate2)
+        elif self.crossover_type == 'ordered':
+            return self.ordered_crossover(candidate1, candidate2)
+        else:
+            raise ValueError("Invalid crossover type")
 
     def replace_highest_cost_solution(self, x):
         highest_cost_index = self.candidates['Cost'].idxmax()
